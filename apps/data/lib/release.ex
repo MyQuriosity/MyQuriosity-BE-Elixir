83888cc65518ca2e7b21688670497c88714cd592
Muhammad Tanweer <tanweerdev@gmail.com>
2025-02-21 18:18:26 +0500

    Add release module

--- apps/data/lib/release.ex
+++ apps/data/lib/release.ex
@@ -1,154 +1,110 @@
-# defmodule MyQampus.Release do
-#   @moduledoc """
-#   # TODO: Write proper moduledoc
-#   """
-#   alias Data.TenantInformation
-#   @db_app :data
-#   @tenant_db_app :tenant_data
-#   @triplex_app :triplex
-#   @e2e_tenant "e2e_school"
-#   @unit_test_tenant "test_sub_domain"
-#   @public_schema "public"
+# credo:disable-for-this-file Credo.Check.Readability.Specs
+defmodule MyQuriosity.Release do
+  @moduledoc false
 
-#   def migrate do
-#     Logger.add_backend(Logger.Backends.Console)
-#     load_app()
+  @start_apps [
+    :crypto,
+    :ssl,
+    :postgrex,
+    :ecto,
+    :ecto_sql
+  ]
 
-#     for repo <- repos() do
-#       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
-#     end
-#   end
+  @app Keyword.fetch!(Mix.Project.config(), :app)
 
-#   # NOTE: This command is only meant to be used in CI-Jenkins E2E pipeline
-#   def re_create_e2e_schemas do
-#     load_and_start_data_apps()
-#     re_create_schema(@e2e_tenant)
-#   end
+  def migrate(args \\ []) do
+    start_services()
+    run_migrations(args)
+    stop_services()
+  end
 
-#   # NOTE: This command is only meant to be used in CI-Jenkins Unit Test pipeline
-#   def re_create_unit_test_schemas do
-#     load_and_start_data_apps()
-#     re_create_schema(@unit_test_tenant)
-#     # re_create_public_schema()
-#   end
+  def rollback(args \\ []) do
+    start_services()
+    run_rollbacks(args)
+    stop_services()
+  end
 
-#   defp load_and_start_data_apps do
-#     Logger.add_backend(Logger.Backends.Console)
-#     load_app()
-#     load_triplex_app()
-#     start_app(@db_app)
-#     start_app(@tenant_db_app)
-#   end
+  def seed(args) do
+    {opts, []} = OptionParser.parse!(args, strict: [file: :string])
+    start_services()
+    run_migrations()
+    run_seeds(Keyword.get(opts, :file, "seeds.exs"))
+    stop_services()
+  end
 
-#   # NOTE: Facing issues while drop public schemas, Maybe we will do it manually.
-#   def re_create_public_schema do
-#     if check_schema_exists?("team_user_oban") do
-#       query = """
-#         DROP SCHEMA team_user_oban CASCADE;
-#       """
+  defp start_services do
+    IO.puts("Starting dependencies..")
+    Enum.each(@start_apps, &Application.ensure_all_started/1)
 
-#       Ecto.Adapters.SQL.query!(Data.Repo, query, [])
-#     end
+    IO.puts("Starting repos..")
+    _load = Application.load(@app)
+    Enum.each(repos(), & &1.start_link(pool_size: 5))
+  end
 
-#     if check_schema_exists?(@public_schema) do
-#       query = """
-#         DROP SCHEMA #{@public_schema} CASCADE;
-#       """
+  defp stop_services do
+    IO.puts("Stopping...")
+    System.stop()
+  end
 
-#       Ecto.Adapters.SQL.query!(Data.Repo, query, [])
-#     end
+  defp run_migrations(args \\ []) do
+    Enum.each(repos(), fn repo ->
+      app = Keyword.get(repo.config(), :otp_app)
+      IO.puts("Running migrations for #{app}")
+      run_migrations_based_on_args(repo, :up, args)
+    end)
+  end
 
-#     query = """
-#       CREATE SCHEMA #{@public_schema};
-#     """
+  defp run_rollbacks(args) do
+    Enum.each(repos(), fn repo ->
+      app = Keyword.get(repo.config(), :otp_app)
+      IO.puts("Running rollback for #{app}")
+      run_migrations_based_on_args(repo, :down, args)
+    end)
+  end
 
-#     Ecto.Adapters.SQL.query!(Data.Repo, query, [])
-#   end
+  defp run_migrations_based_on_args(repo, direction, args) do
+    case args do
+      ["--step", n] -> run_migrations_for(repo, direction, step: String.to_integer(n))
+      ["-n", n] -> run_migrations_for(repo, direction, step: String.to_integer(n))
+      ["--to", to] -> run_migrations_for(repo, direction, to: String.to_integer(to))
+      ["--all"] -> run_migrations_for(repo, direction, all: true)
+      [] -> run_migrations_for(repo, direction)
+    end
+  end
 
-#   defp check_schema_exists?(schema) do
-#     query = """
-#     SELECT schema_name FROM information_schema.schemata WHERE schema_name = '#{schema}';
-#     """
+  defp run_migrations_for(repo, :up), do: run_migrations_for(repo, :up, all: true)
+  defp run_migrations_for(repo, :down), do: run_migrations_for(repo, :down, step: 1)
 
-#     %Postgrex.Result{num_rows: num_rows} = Ecto.Adapters.SQL.query!(Data.Repo, query, [])
-#     num_rows > 0
-#   end
+  defp run_migrations_for(repo, direction, opts) do
+    migrations_path = priv_path_for(repo, "migrations")
+    Ecto.Migrator.run(repo, [migrations_path], direction, opts)
+  end
 
-#   defp re_create_schema(schema) do
-#     if Triplex.exists?(schema) do
-#       Triplex.drop(schema)
-#     end
+  defp run_seeds(seed_file), do: Enum.each(repos(), &run_seeds_for(&1, seed_file))
 
-#     # Sleep 2 seconds to finish dropping of schema
-#     Process.sleep(2_000)
-#     Triplex.create(schema)
-#     # Sleep 5 seconds to finish creating of schema
-#     Process.sleep(5_000)
-#   end
+  defp run_seeds_for(repo, seed_file) do
+    # Run the seed script if it exists
+    seed_script = priv_path_for(repo, seed_file)
 
-#   def create_e2e_tenant_info do
-#     load_and_start_data_apps()
-#     params = tenant_info_params()
+    if File.exists?(seed_script) do
+      IO.puts("Running seed script #{seed_script}..")
+      Code.eval_file(seed_script)
+    else
+      IO.puts("Seed script #{seed_script} does not exist..")
+    end
+  end
 
-#     %TenantInformation{}
-#     |> TenantInformation.changeset(params)
-#     |> Data.Repo.insert()
-#   end
+  defp priv_path_for(repo, filename) do
+    app_dir = Application.app_dir(Keyword.fetch!(repo.config, :otp_app))
 
-#   defp tenant_info_params do
-#     %{
-#       sub_domain: @e2e_tenant,
-#       tenant_prefix: @e2e_tenant
-#     }
-#   end
+    repo_underscore =
+      repo
+      |> Module.split()
+      |> List.last()
+      |> Macro.underscore()
 
-#   def seed_admin do
-#     load_app()
-#     start_app(@db_app)
-#     team_admin_password = System.fetch_env!("TEAM_ADMIN_PASSWORD")
+    Path.join([app_dir, "priv", repo_underscore, filename])
+  end
 
-#     data = [
-#       %{
-#         first_name: "Super",
-#         last_name: "Admin",
-#         username: "admin",
-#         hashed_password: Core.Utils.AuthUtils.hash_password(team_admin_password),
-#         email: "admin@gmail.com",
-#         phone: "03001234567",
-#         gender: "male",
-#         is_admin: true,
-#         inserted_at: DateTime.truncate(DateTime.utc_now(), :second),
-#         updated_at: DateTime.truncate(DateTime.utc_now(), :second)
-#       }
-#     ]
-
-#     Data.Repo.insert_all(Data.TeamUser, data,
-#       on_conflict: {:replace_all_except, [:id]},
-#       returning: true,
-#       conflict_target: [:id]
-#     )
-#   end
-
-#   def rollback(repo, version) do
-#     load_app()
-#     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
-#   end
-
-#   defp repos do
-#     Application.fetch_env!(@db_app, :ecto_repos)
-#   end
-
-#   defp load_app do
-#     Application.load(@db_app)
-#   end
-
-#   defp load_triplex_app do
-#     Application.load(@triplex_app)
-#     Application.load(@tenant_db_app)
-#   end
-
-#   defp start_app(app) do
-#     Application.put_env(app, :minimal, true)
-#     Application.ensure_all_started(app)
-#   end
-# end
+  defp repos, do: Application.fetch_env!(@app, :ecto_repos)
+end
