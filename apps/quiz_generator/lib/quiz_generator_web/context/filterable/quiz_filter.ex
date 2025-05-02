8c0867e9f2fb83cd7519c2f1c2995ae3a90d56c7
Muhammad Tanweer <tanweerdev@gmail.com>
2025-05-02 12:53:23 +0500

    Try FatEcto version 1.2

--- apps/quiz_generator/lib/quiz_generator_web/context/filterable/quiz_filter.ex
+++ apps/quiz_generator/lib/quiz_generator_web/context/filterable/quiz_filter.ex
@@ -1,17 +1,17 @@
 defmodule QuizGeneratorWeb.Filterable.QuizFilter do
   @moduledoc """
-    A dynamic filtering module for the `Quiz` schema using `FatEcto.Dynamics.FatBuildable`.
+    A dynamic filtering module for the `Quiz` schema using `FatEcto.Builder.FatDynamicsBuildable`.
   """
 
   import Ecto.Query
 
-  use FatEcto.Dynamics.FatBuildable,
-    filterable_fields: %{
-      "title" => "$ILIKE",
-      "topic_id" => "$EQUAL",
-      "id" => "$EQUAL"
-    },
-    overrideable_fields: [
+  use FatEcto.Builder.FatDynamicsBuildable,
+    filterable: [
+      title: "$ILIKE",
+      topic_id: "$EQUAL",
+      id: "$EQUAL"
+    ],
+    overrideable: [
       "syllabus_provider_id",
       "grade_id",
       "subject_id",
@@ -19,15 +19,15 @@ defmodule QuizGeneratorWeb.Filterable.QuizFilter do
       "topic_title",
       "inserted_at"
     ],
-    ignoreable_fields_values: %{
-      "title" => ["%%", nil],
-      "topic_title" => ["%%", nil],
-      "topic_id" => ["", nil],
-      "subject_id" => ["", nil],
-      "chapter_id" => ["", nil],
-      "inserted_at" => ["", nil],
-      "id" => ["", nil]
-    }
+    ignoreable: [
+      title: ["%%", nil],
+      topic_title: ["%%", nil],
+      topic_id: ["", nil],
+      subject_id: ["", nil],
+      chapter_id: ["", nil],
+      inserted_at: ["", nil],
+      id: ["", nil]
+    ]
 
   @impl true
   def after_whereable(dynamics) do
@@ -35,25 +35,25 @@ defmodule QuizGeneratorWeb.Filterable.QuizFilter do
   end
 
   @impl true
-  def override_whereable(_dynamics, "syllabus_provider_id", "$EQUAL", value) do
+  def override_buildable(_dynamics, "syllabus_provider_id", "$EQUAL", value) do
     dynamic([syllabus_provider: syllabus_provider], syllabus_provider.id == ^value)
   end
 
-  def override_whereable(_dynamics, "grade_id", "$EQUAL", value) do
+  def override_buildable(_dynamics, "grade_id", "$EQUAL", value) do
     dynamic([grade: grade], grade.id == ^value)
   end
 
-  def override_whereable(_dynamics, "subject_id", "$EQUAL", value) do
+  def override_buildable(_dynamics, "subject_id", "$EQUAL", value) do
     dynamic([subject: subject], subject.id == ^value)
   end
 
-  def override_whereable(_dynamics, "chapter_id", "$EQUAL", value) do
+  def override_buildable(_dynamics, "chapter_id", "$EQUAL", value) do
     dynamic([chapter: chapter], chapter.id == ^value)
   end
 
-  def override_whereable(_dynamics, "topic_title", "$ILIKE", value) do
+  def override_buildable(_dynamics, "topic_title", "$ILIKE", value) do
     dynamic([topic: topic], ilike(fragment("(?)::TEXT", topic.title), ^value))
   end
 
-  def override_whereable(dynamics, _field, _operator, _value), do: dynamics
+  def override_buildable(dynamics, _field, _operator, _value), do: dynamics
 end
